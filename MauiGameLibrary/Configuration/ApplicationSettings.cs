112184b98c3cbec9c3368fb9d4af3bdcae3ffc0d
adpead <adpead@gmail.com>
2025-09-10 21:13:49 +0200

    Added Maui project Working up until the list view

---
+++ MauiGameLibrary/Configuration/ApplicationSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiGameLibrary.Configuration
+{
+    //   https://localhost:7055/api/GameStuff
+    public class ApplicationSettings
+    {
+        public string ServerName { get; set; }
+        public int Port { get; set; }
+        public string BaseRoute { get; set; }
+
+        public string ServiceUrl { get; set; }
+
+        public ApplicationSettings()
+        {
+            ServerName = "localhost";
+
+#if DEBUG
+
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+                ServerName = "10.0.2.2";
+#endif
+
+            Port = 7055;
+            BaseRoute = "api/GameStuff";
+            ServiceUrl = $"https://{ServerName}:{Port}/{BaseRoute}";
+        }
+
+    }
+}
---
+++ MauiGameLibrary/MauiProgram.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Maui;
+using MauiGameLibrary.Configuration;
+using MauiGameLibrary.Interfaces;
+using MauiGameLibrary.Services;
+using MauiGameLibrary.ViewModels;
+using MauiGameLibrary.Views;
+using Microsoft.Extensions.Logging;
+
+namespace MauiGameLibrary
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddTransient<ListOfGamesView>();
+            builder.Services.AddTransient<ListOfGamesViewModel>();
+
+            builder.Services.AddTransient<UpdateGameView>();
+            builder.Services.AddTransient<UpdateGameViewModel>();
+
+
+            builder.Services.AddSingleton<IGameService,GameDataApiService>();
+            builder.Services.AddSingleton<ApplicationSettings>();
+
+            return builder.Build();
+        }
+    }
+}
