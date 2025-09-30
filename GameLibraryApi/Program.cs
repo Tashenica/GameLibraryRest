795ce3d7e3e4042ebecde749fb5e07c2c552142a
adpead <adpead@gmail.com>
2025-09-30 12:43:16 +0200

    Ensure Auto Migration.

--- GameLibraryApi/Program.cs
+++ GameLibraryApi/Program.cs
@@ -33,6 +33,7 @@ using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<GamingLibraryContext>();
     context.Database.EnsureCreated(); // Create the database
+    context.Database.Migrate();  //Apply any pending migrations
     context.SeedData(); // Seed with sample data
 }
 
